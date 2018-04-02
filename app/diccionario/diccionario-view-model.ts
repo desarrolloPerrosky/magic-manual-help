953053d359ebc0127966140e4f9f66ef2b13fe8f
Hector Niñez <desarrollo.perrosky@gmail.com>
2018-04-02 02:07:59 -0300

    Mensajeria de diccionario v 0.001

--- app/diccionario/diccionario-view-model.ts
+++ app/diccionario/diccionario-view-model.ts
@@ -1,6 +1,6 @@
 import { Observable } from "data/observable";
 
-export class BrowseViewModel extends Observable {
+export class DiccionarioViewModel extends Observable {
 	private _navigationItems: Array<any>;
 	
     constructor() {
@@ -12,86 +12,72 @@ export class BrowseViewModel extends Observable {
                 name: "home",
                 route: "home/home-page",
                 icon: "\uf2cd",
-            },
-            {
+            },{
                 title: "Browse",
                 name: "browse",
                 route: "browse/browse-page",
                 icon: "\uf1ea",
-            },
-            {
+            },{
                 title: "Search",
                 name: "search",
                 route: "search/search-page",
                 icon: "\uf002",
-            },
-            {
+            },{
                 title: "Featured",
                 name: "featured",
                 route: "featured/featured-page",
                 icon: "\uf005",
-            },
-            {
+            },{
                 title: "Diccionario",
                 name: "diccionario",
                 route: "diccionario/diccionario-page",
                 icon: "\uf02d",
-            },			
-            {
+            },{
                 title: "Settings",
                 name: "settings",
                 route: "settings/settings-page",
                 icon: "\uf013",
-            },
-            {
+            },{
                 title: "Casita",
                 name: "home",
                 route: "home/home-page",
                 icon: "\uf2cd",
-            },
-            {
+            },{
                 title: "Browse",
                 name: "browse",
                 route: "browse/browse-page",
                 icon: "\uf1ea",
-            },
-            {
+            },{
                 title: "Search",
                 name: "search",
                 route: "search/search-page",
                 icon: "\uf002",
-            },
-            {
+            },{
                 title: "Featured",
                 name: "featured",
                 route: "featured/featured-page",
                 icon: "\uf005",
-            },
-            {
+            },{
                 title: "Diccionario",
                 name: "diccionario",
                 route: "diccionario/diccionario-page",
                 icon: "\uf02d",
-            },			
-            {
+            },{
                 title: "Settings",
                 name: "settings",
                 route: "settings/settings-page",
                 icon: "\uf013",
-            },
-            {
+            },{
                 title: "Casita",
                 name: "home",
                 route: "home/home-page",
                 icon: "\uf2cd",
-            },
-            {
+            },{
                 title: "Browse",
                 name: "browse",
                 route: "browse/browse-page",
                 icon: "\uf1ea",
-            },
-            {
+            },{
                 title: "Search",
                 name: "search",
                 route: "search/search-page",
@@ -226,6 +212,30 @@ export class BrowseViewModel extends Observable {
 		];
     }
 	
+    public onSubmit(args) {
+//        let searchBar = <SearchBar>args.object;
+//        let searchValue = searchBar.text.toLowerCase();
+//
+//        this.myItems = new ObservableArray<DataItem>();
+//        if (searchValue !== "") {
+//            for (let i = 0; i < this.arrayItems.length; i++) {
+//                if (this.arrayItems[i].name.toLowerCase().indexOf(searchValue) !== -1) {
+//                    this.myItems.push(this.arrayItems[i]);
+//                }
+//            }
+//        }
+    }
+
+    public onClear(args) {
+//        let searchBar = <SearchBar>args.object;
+//        searchBar.text = "";
+//        searchBar.hint = "Search for a country and press enter";
+//
+//        this.myItems = new ObservableArray<DataItem>();
+//        this.arrayItems.forEach(item => {
+//            this.myItems.push(item);
+//        });
+    }
 	
     get navigationItems(): Array<any> {
         return this._navigationItems;
